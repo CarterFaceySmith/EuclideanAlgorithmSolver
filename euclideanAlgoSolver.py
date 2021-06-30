d7c5bcb95529a85e6bede818ec4963423006ef0c
Carter Facey-Smith <43100930+CarterFaceySmith@users.noreply.github.com>
2021-07-01 09:13:36 +1000

    Add files via upload

---
+++ euclideanAlgoSolver.py
@@ -0,0 +1,35 @@
+print('--------------------------------------')
+print('------EUCLIDEAN ALGORITHM SOLVER------')
+print('--------------------------------------\n\n')
+
+num1 = int(input ('Enter the first number: \n'))
+num2 = int(input ('Enter the second number: \n'))
+
+#Error handling for if zero is given for input
+if num1 == 0:
+    print('Invalid input for your first number\n')
+if num2 == 0:
+    print('Invalid input for your first number\n')
+else:
+    #Error handling for if numbers are mispositioned
+    if num2 > num1:
+        tempVar = num1
+        num1 = num2
+        num2 = tempVar
+    
+    #Euclid's Algorithm
+    else:
+        while num2 != 0:
+            temp1 = num2
+            temp2 = num1 % num2
+            num2 = temp2
+            num1 = temp1
+    
+    num1 = str(num1)
+    num2= str(num2)
+    print('Program done.')
+    print('The GCF is ' + num1)
+            
+
+
+# print('\t' + nums)
