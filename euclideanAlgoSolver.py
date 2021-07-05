aed546c852a9fd2560c8f36821e4389636496adf
Carter Facey-Smith <43100930+CarterFaceySmith@users.noreply.github.com>
2021-07-05 13:30:07 +1000

    Add files via upload
    - Added complexity and cycle count to result
    - Added system exit to invalid inputs

--- euclideanAlgoSolver.py
+++ euclideanAlgoSolver.py
@@ -1,15 +1,21 @@
+import sys
+
 print('--------------------------------------')
 print('------EUCLIDEAN ALGORITHM SOLVER------')
 print('--------------------------------------\n\n')
 
 num1 = int(input ('Enter the first number: \n'))
 num2 = int(input ('Enter the second number: \n'))
+count = 0
 
 #Error handling for if zero is given for input
 if num1 == 0:
     print('Invalid input for your first number\n')
+    sys.exit(0)
 if num2 == 0:
     print('Invalid input for your first number\n')
+    sys.exit(0)
+    
 else:
     #Error handling for if numbers are mispositioned
     if num2 > num1:
@@ -24,8 +30,13 @@ else:
             temp2 = num1 % num2
             num2 = temp2
             num1 = temp1
+            count += 1
     
     num1 = str(num1)
     num2= str(num2)
+    newCount = count
+    newCount = str(newCount)
     print('Program done.')
     print('The GCF is ' + num1)
+    print('Result obtained in: ' + newCount + ' cycles.')
+    print('Resulting in a complexity of O(' + newCount + ').')
